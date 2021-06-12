5253edfd416ee11b528b95223c515dda92e7dca5
Tinotenda Musikavanhu <thesafehavenhero@gmail.com>
2021-06-11 20:20:02 -0500

    update

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,21 @@
 const db = require("../models");
 const router = require("express").Router();
 
+//commnet in to prepopulate database
+// db.Workout.find({}).then(function (res) {
+//     console.log("Checking if db is populated");
+//     if (res.length === 0) {
+//         console.log("DB is empty");
+//         require("./seeders/seed.js");
+//     }
+// });
 
+//get workouts
 router.get("/api/workouts", (req, res) => {
+
     db.Workout.find({}).then(dbWorkout => {
+        // console.log("ALL WORKOUTS");
+        // console.log(dbWorkout);
         dbWorkout.forEach(workout => {
             var total = 0;
             workout.exercises.forEach(e => {
@@ -18,7 +30,10 @@ router.get("/api/workouts", (req, res) => {
         res.json(err);
     });
 });
+
+// add exercise
 router.put("/api/workouts/:id", (req, res) => {
+
     db.Workout.findOneAndUpdate(
         { _id: req.params.id },
         {
@@ -33,13 +48,19 @@ router.put("/api/workouts/:id", (req, res) => {
 
 });
 
+//create workout
 router.post("/api/workouts", ({ body }, res) => {
+    // console.log("WORKOUT TO BE ADDED");
+    // console.log(body);
+
     db.Workout.create(body).then((dbWorkout => {
         res.json(dbWorkout);
     })).catch(err => {
         res.json(err);
     });
 });
+
+// get workouts in range
 router.get("/api/workouts/range", (req, res) => {
 
     db.Workout.find({}).then(dbWorkout => {
@@ -52,4 +73,6 @@ router.get("/api/workouts/range", (req, res) => {
     });
 
 });
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
@@ -23,4 +25,6 @@ mongoose.connect(
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
