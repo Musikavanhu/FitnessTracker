dffbcd1eb2edb83c84be4860a20e4591347ef940
Tinotenda Musikavanhu <thesafehavenhero@gmail.com>
2021-06-11 19:54:47 -0500

    update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,6 +23,6 @@ mongoose.connect(
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
 });
